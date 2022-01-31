da26b1146ce8580536df41eaf0cf9e950dfacf05
fusbudget <andrewsanser@gmail.com>
2022-01-31 15:41:15 +0100

    add input validation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,43 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEmailInput = this.handleEmailInput.bind(this);
+    this.handlePwdInput = this.handlePwdInput.bind(this);
+  }
+
+  handleEmailInput(e) {
+    let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/; 
+    if (e.target.value.match(pattern)){
+      e.target.className = "form-control is-valid"; 
+    } else {
+      e.target.className = "form-control is-invalid";
+    }
+  }
+
+  handlePwdInput(e) {
+    let pattern =  /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    if (e.target.value.match(pattern)){
+      e.target.className = "form-control is-valid"; 
+    } else {
+      e.target.className = "form-control is-invalid";
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log('handlesubmit')
+  }
+
   render() {
     return(
       <div className="container">
         <div className="row d-flex justify-content-center">
           <div className="col col-lg-6 ">
             <h1 className="text-center">Login</h1>
-            <form class="">
+            <form onSubmit={this.handleSubmit}>
               <label for="email" className="form-label mt-2">
                 Email address
               </label>
@@ -18,6 +48,7 @@ class App extends React.Component {
                 id="email"
                 placeholder="Enter email..."
                 required
+                onChange={this.handleEmailInput}
               />
               <label for="password" className="form-label mt-2">
                 Password
@@ -28,9 +59,10 @@ class App extends React.Component {
                 id="password"
                 placeholder="Enter password..."
                 required
+                onChange={this.handlePwdInput}      
               />
               <div className="mt-2 form-check">
-                <input type="checkBox" className="form-check-input" id="check" />
+                <input type="checkBox" className="form-check-input" id="checkBox" />
                 <label className="form-check-label" for="checkBox">
                   Remember me
                 </label>
